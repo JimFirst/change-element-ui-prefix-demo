42b2f5e7acd1501599514a3647b457d339a63c85
董铭杰 <dongmj@getui.com>
2023-07-04 11:56:21 +0800

    [update]: 新增ElImageViewer使用示范

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+    
     <div>
       element-ui的组件前缀和样式前缀都被替换成gp了，请打开控制台查看。
     </div>
@@ -24,16 +33,32 @@
       type="date"
       placeholder="选择日期">
     </el-date-picker>
+    <ElImageViewer
+      v-if="imageViewerVisible"
+      :on-close="closeImageView"
+      :url-list="srcList"
+    ></ElImageViewer>
+    <el-image 
+      style="width: 100px; height: 100px"
+      src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" 
+      :preview-src-list="srcList">
+    </el-image>
   </div>
 </template>
 
 <script>
+import { Image } from 'element-ui'
 export default {
   name: 'HelloWorld',
+  components: {
+    ElImageViewer: Image.components.ImageViewer
+  },
   data() {
     return {
       value: '',
-      time: ''
+      time: '',
+      srcList: ['https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'],
+      imageViewerVisible: true
     }
   },
   methods: {
@@ -53,7 +78,45 @@ export default {
           message: '已取消删除'
         });          
       });
+    },
+    closeImageView() {
+      this.imageViewerVisible = false
     }
   }
 }
 </script>
+<style>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
